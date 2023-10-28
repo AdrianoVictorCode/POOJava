919bf786dd2118c1a6bcb599f13beabb59bf7af3
AdrianoVictorCode <129338740+AdrianoVictorCode@users.noreply.github.com>
2023-10-28 10:47:13 -0300

    New Commits

--- GerenciamentoEscolar/src/App.java
+++ GerenciamentoEscolar/src/App.java
@@ -1,25 +1,39 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        Pessoa p1 = new Pessoa();
-        Aluno p2 = new Aluno();
-        Professor p3 = new Professor();
-        Turma t1 = new Turma(1);
+        Escola escola = new Escola();
+        Professor professor1 = new Professor("Professor 1", "Matemática", 35, "M");
+        Professor professor2 = new Professor("Professor 2", "História", 40, "F");
+        Alunos aluno1 = new Alunos("Aluno 1", 15, "M");
+        Alunos aluno2 = new Alunos("Aluno 2", 16, "F");
+        Alunos aluno3 = new Alunos("Aluno 3", 17, "M");
+        Turma turma1 = new Turma("Turma A", professor1);
+        Turma turma2 = new Turma("Turma B", professor2);
 
-        t1.adicionarTurma("Ingles");
-        
-        t1.adicionarTurma("Matematica");
-        p1.setNome("Pedro");
-        p2.setNome("Maria");
-        p3.setNome("Claudio");
-        
-        p1.setSexo("Masculino");
-        p2.setIdade(18);
+        escola.adicionarProfessor(professor1);
+        escola.adicionarProfessor(professor2);
+        escola.adicionarAluno(aluno1);
+        escola.adicionarAluno(aluno2);
+        escola.adicionarAluno(aluno3);
+        escola.adicionarTurma(turma1);
+        escola.adicionarTurma(turma2);
 
-        p2.setCurso("Informatica");
-        p3.setSalario(2500.75f);
-        //System.out.println(p1.toString());
-        //System.out.println(p2.toString());
-        //System.out.println(p3.toString());
+        turma1.adicionarAluno(aluno1);
+        turma1.adicionarAluno(aluno2);
+        turma2.adicionarAluno(aluno2);
+        turma2.adicionarAluno(aluno3);
 
+        System.out.println("Turmas ministradas pelo Professor:");
+        ArrayList<Turma> turmasProfessor1 = escola.turmasDoProfessor(professor1);
+        for (Turma turma : turmasProfessor1) {
+            System.out.println(turma.getNomeTurma());
+        }
+
+        System.out.println("\nAlunos na Turma B:");
+        ArrayList<Alunos> alunosTurmaB = escola.alunosDaTurma(turma2);
+        for (Alunos aluno : alunosTurmaB) {
+            System.out.println(aluno.getNomeAluno());
+        }
     }
 }
--- GerenciamentoEscolar/src/Turma.java
+++ GerenciamentoEscolar/src/Turma.java
@@ -1,16 +1,42 @@
 import java.util.ArrayList;
 
 public class Turma {
-    private int numeroTurma;
-    private Aluno[] alunos;
-    private ArrayList <String> turmas;
+    private String nomeTurma;
+    private Professor professor;
+    private ArrayList<Alunos> alunos;
 
-    public Turma(int numeroTurma){
-        this.numeroTurma = numeroTurma;
+    public Turma(String nomeTurma, Professor professor) {
+        this.setNomeTurma(nomeTurma);
+        this.setProfessor(professor);
+        this.alunos = new ArrayList<>();
     }
 
-    public void adicionarTurma (String nomeTurma){
-        turmas.add(nomeTurma);
+    public String getNomeTurma() {
+        return nomeTurma;
+    }
+    public void setNomeTurma(String nomeTurma) {
+        this.nomeTurma = nomeTurma;
     }
 
+    public void adicionarAluno(Alunos aluno) {
+        alunos.add(aluno);
+        aluno.adicionarTurma(this);
+    }
+
+
+
+    public ArrayList<Alunos> getAlunos() {
+        return alunos;
+    }
+    public void setAlunos(ArrayList<Alunos> alunos) {
+        this.alunos = alunos;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
+    public Professor getProfessor() {
+        return professor;
+    }
 }
+
